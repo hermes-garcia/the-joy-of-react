6b1a5285ddeb205fdbfeb87b181c5dc06fb4ec09
Hermes <hermes.garcia@bounteous.com>
2023-02-23 21:59:45 -0600

    Project 1 - first exercise

---
+++ 01-word-clone/src/components/GuessInput/GuessInput.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function GuessInput() {
+
+    const [word, setWord] = React.useState('');
+
+    const handleWordSubmit = (ev) => {
+        ev.preventDefault();
+        console.log(word);
+        setWord('');
+    };
+
+    return (
+        <form className="guess-input-wrapper" onSubmit={handleWordSubmit}>
+            <label htmlFor="guess-input">Enter guess:</label>
+            <input
+                required
+                id="guess-input"
+                type="text"
+                pattern="[A-Za-z]{5}"
+                maxLength="5"
+                placeholder="5 letter word"
+                value={word}
+                onChange={({target}) => {
+                    const word = target.value;
+                    setWord(word.toUpperCase());
+                }}
+            />
+        </form>
+    );
+}
+
+export default GuessInput;
